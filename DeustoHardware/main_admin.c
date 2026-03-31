2b55c8d4bd1cecb985e6f9c263c2445773f5959e
oierpalacios-del <oier.palacios@opendeusto.es>
2026-03-31 08:51:26 +0200

    anyadido includes e inicializacion de base de datos en el admin

--- DeustoHardware/main_admin.c
+++ DeustoHardware/main_admin.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "db_manager.h"
+#include "sqlite3.h"
+#include "main_admin.h"
+int baseDeDatos(){
+	sqlite3 *db;
+
+	    printf("Arrancando el servidor de Deusto Hardware...\n");
+
+	    // Esto abre o crea el fichero de la BD
+	    int resultado = sqlite3_open("data/deusto_hardware.sqlite", &db); //
+
+	    if (resultado != SQLITE_OK) { //
+	        printf("Error fatal al abrir la base de datos.\n");
+	        return 1;
+	    }
+
+	    // Crea las tablas de la BD
+	    inicializar_base_datos(db);
+
+	    // TODO Código para la gestión de conexión
+
+	    sqlite3_close(db);
+
+	    return 0;
+}
 void menu(){
 	char str[50];
 	int opcion;
